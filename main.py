0fd5f1edca2e8eae348091ce872b6d3db36f49f9
Shivam Prajapati <128581121+ShivamDeadlocker@users.noreply.github.com>
2024-10-03 10:58:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("This is main.py changes done by Shivam")
+def to_upper(name):
+     return name.upper()
+ 
+def say_hello(name):
+    print('Hello,{name}')
+
+if __name__ == '__main__':
+    name='shivam'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
